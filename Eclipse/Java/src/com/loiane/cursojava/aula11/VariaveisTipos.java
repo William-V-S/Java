fe32be63f6e4381afc3b54b8299d6b26effd586d
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-11-13 15:55:59 -0300

    aula 11

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula11/VariaveisTipos.java
@@ -0,0 +1,24 @@
+package com.loiane.cursojava.aula11;
+
+public class VariaveisTipos {
+
+	public static void main(String[] args) {
+		// inteiros
+		byte inteiro = 20;
+		short inteiro2 = 20;
+		int inteiro3 = 20;	// usa
+		long inteiro4 = 20;	// usa
+		char i = 105;// 105 = "i"
+		//Pontos Flutuantes 
+		float quebrados = 36.5f;
+		double quebrados2 = 36.5; //favorito
+		//Boolean(usa)
+		boolean verdadeiro = true;
+		boolean falso = false;
+		System.out.println("inteiros: " + inteiro + inteiro2 + inteiro3 + inteiro4 + i  );
+		System.out.println("Quebrados: " + quebrados + quebrados2);
+		System.out.println("Vendadeiro = " + verdadeiro + " e falso é = " + falso);
+		
+	}
+
+}
