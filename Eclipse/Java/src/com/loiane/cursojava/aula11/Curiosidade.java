1dfbd5c786eeed8d7409627b1e25a9a720d003b3
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-11-13 16:01:00 -0300

    curiosidade

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula11/Curiosidade.java
@@ -0,0 +1,13 @@
+package com.loiane.cursojava.aula11;
+
+public class Curiosidade {
+
+	public static void main(String[] args) {
+		
+		int var1 = 2147483647;// numero maximo
+		int var2 = 50;
+		System.out.println(var1 + var2);
+		//apos o numero maximo começa a contar os negativos
+	}
+
+}
