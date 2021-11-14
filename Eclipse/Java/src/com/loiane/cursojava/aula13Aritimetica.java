79424515f2ec74603d3d582db3cb8605437902bf
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-11-14 17:24:12 -0300

    Create aula13Aritimetica.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula13Aritimetica.java
@@ -0,0 +1,31 @@
+package com.loiane.cursojava;
+
+public class aula13Aritimetica {
+
+	public static void main(String[] args) {
+		int resultado = 5+3;
+		System.out.println(resultado);
+		
+		resultado = resultado * 3;
+		System.out.println(resultado);
+		
+		resultado = resultado /2;
+		System.out.println(resultado);
+		
+		resultado= resultado%5;
+		System.out.println(resultado);
+		
+		resultado = resultado+1;
+		System.out.println(resultado);
+		resultado++;
+		System.out.println(resultado);
+		
+		System.out.println(resultado++);
+		System.out.println(++resultado);
+		
+		System.out.println(resultado--);
+		System.out.println(--resultado);
+		
+	}
+
+}
