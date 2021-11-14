631d12514fa0228ba5428475bd3cfb08db8598a3
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-11-14 17:07:58 -0300

    Create Aula12.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/Aula12.java
@@ -0,0 +1,27 @@
+package com.loiane.cursojava;
+
+import java.util.Scanner;
+
+public class Aula12 {
+
+	public static void main(String[] args) {
+		Scanner escanear = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome completo: ");
+		String nomeCompleto = escanear.nextLine();
+		System.out.println("Seu nome completo é: " + nomeCompleto);
+		
+		System.out.println("Digite seu nome completo: ");
+		String primeiroNome = escanear.next();
+		System.out.println("Seu primeiro nome é: " + primeiroNome);
+		
+		System.out.println("Digite Sua Idade: ");
+		int idade = escanear.nextInt();
+		System.out.println("Sua idade é: "+idade);
+		
+		System.out.println("Digite Sua altura: ");
+		double altura = escanear.nextDouble();
+		System.out.println("Sua altura é: "+ altura);
+	}
+
+}
