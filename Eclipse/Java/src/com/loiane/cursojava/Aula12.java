e79e5da50e3a28e3754b5c644324be06df049d45
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-11-15 20:10:17 -0300

    Update Aula12.java

--- Eclipse/Java/src/com/loiane/cursojava/Aula12.java
+++ Eclipse/Java/src/com/loiane/cursojava/Aula12.java
@@ -12,12 +12,12 @@ public class Aula12 {
 		System.out.println("Seu nome completo é: " + nomeCompleto);
 		
 		System.out.println("Digite seu nome completo: ");
-		String primeiroNome = escanear.next();
+		String primeiroNome = escanear.nextLine();
 		System.out.println("Seu primeiro nome é: " + primeiroNome);
 		
 		System.out.println("Digite Sua Idade: ");
 		int idade = escanear.nextInt();
-		System.out.println("Sua idade é: "+idade);
+		System.out.println("Sua idade é: " + idade);
 		
 		System.out.println("Digite Sua altura: ");
 		double altura = escanear.nextDouble();
