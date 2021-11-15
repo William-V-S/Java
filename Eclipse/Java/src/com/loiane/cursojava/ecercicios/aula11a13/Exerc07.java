ae1b5f3a55157bbff8ec49cb2b00f7faf4c75961
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-11-15 20:16:21 -0300

    Create Exerc07.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/ecercicios/aula11a13/Exerc07.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.ecercicios.aula11a13;
+
+import java.util.Scanner;
+
+public class Exerc07 {
+
+	public static void main(String[] args) {
+		Scanner scam = new Scanner(System.in);
+		System.out.println("Dobro da Area do Quadrado");
+		System.out.println("Digite quanto tem o lado: ");
+		double lado = scam.nextDouble();
+		double area = lado*lado;
+		double dobro = area*2;
+		System.out.println("Area: "+area);
+		System.out.println("Dobro da area: "+dobro);
+	}
+
+}
