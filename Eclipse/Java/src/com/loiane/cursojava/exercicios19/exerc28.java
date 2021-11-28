db27cf09e5b0750c5ba41d80bf3e8434cad902fd
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-11-28 20:31:43 -0300

    Create exerc28.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/exercicios19/exerc28.java
@@ -0,0 +1,27 @@
+package com.loiane.cursojava.exercicios19;
+
+public class exerc28 {
+
+	public static void main(String[] args) {
+		int[] vetorA = new int[10];
+		int[] vetorB = new int[10];
+		for (int i = 1 ,j = 0; i <= 20; i+=2 ,j++) {
+			vetorA[j] += i;
+		}
+		for (int i = 9 ,j = 0; i >= 0; i-- ,j++) {
+	//		System.out.println(i);
+	//		System.out.println(j);
+			
+			vetorB[j] += vetorA[i];
+		}
+		for (int i : vetorA) {
+			System.out.println(i);
+		}
+		for (int i : vetorB) {
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
