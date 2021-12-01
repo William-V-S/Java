f006a98d2a306f7f258229f61f20eab94d0df303
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-01 19:45:15 -0300

    Create Aula21FoeEach.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/Aula21FoeEach.java
@@ -0,0 +1,20 @@
+package com.loiane.cursojava;
+
+import java.util.Random;
+
+public class Aula21FoeEach {
+
+	public static void main(String[] args) {
+		int[] notas = new int[10];
+		Random random = new Random();
+		for (int i = 0; i < notas.length; i++) {
+			notas[i] = random.nextInt(10);
+		}
+		
+		for (int nota :notas) {
+			System.out.println(nota);
+		}
+
+	}
+
+}
