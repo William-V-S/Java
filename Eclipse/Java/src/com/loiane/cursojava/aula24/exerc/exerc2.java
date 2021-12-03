39b8a44f16ac61d30f7c793b03d069659f7aa5ef
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-03 18:01:19 -0300

    Create exerc2.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula24/exerc/exerc2.java
@@ -0,0 +1,19 @@
+package com.loiane.cursojava.aula24.exerc;
+
+public class exerc2 {
+
+	public static void main(String[] args) {
+		Livro livro = new Livro();
+		livro.nome= "O Maior Segredo do mundo";
+		livro.autor="Mariazinha Galvão";
+		livro.ano=1972;
+		livro.paginas=164;
+		
+		System.out.println("Nome do livro: "+livro.nome);
+		System.out.println("Autor: "+livro.autor);
+		System.out.println("Ano: "+livro.ano);
+		System.out.println("paginas: "+livro.ano);
+
+	}
+
+}
