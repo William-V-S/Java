0f78a625d975050b89e7ef53e293a21dde4f8c73
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-07 20:26:47 -0300

    Create carro2.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula29/carro2.java
@@ -0,0 +1,20 @@
+package com.loiane.cursojava.aula29;
+
+public class carro2 {
+	String marca;
+	String modelo;
+	int passageiros;
+	double combustivel;
+	double consumo;
+	public carro2(String marca, String modelo, int passageiros, double combustivel, double consumo) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.passageiros = passageiros;
+		this.combustivel = combustivel;
+		this.consumo = consumo;
+	}
+	public carro2() { }
+	
+	
+}
