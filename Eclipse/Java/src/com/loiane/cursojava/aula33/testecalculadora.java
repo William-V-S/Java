798d44166a1790ac90c62297e05a54bcc525a0fa
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-08 20:29:18 -0300

    Create testecalculadora.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula33/testecalculadora.java
@@ -0,0 +1,13 @@
+package com.loiane.cursojava.aula33;
+
+public class testecalculadora {
+
+	public static void main(String[] args) {
+		calculadora calc = new calculadora();
+		
+		calc.soma(2, 4);
+		calc.soma(2.3, 4.5);
+
+	}
+
+}
