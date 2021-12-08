ead1d5b71aa36d40ca9a23473a2a7f5539daa280
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-08 20:04:43 -0300

    Create Sera.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula32/Sera.java
@@ -0,0 +1,24 @@
+package com.loiane.cursojava.aula32;
+
+public class Sera {
+
+	private String marca;
+	private String modelo;
+	private int passageiros;
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	 
+
+}
