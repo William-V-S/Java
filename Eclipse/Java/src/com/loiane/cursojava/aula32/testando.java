2dfd300a88db3f4a2e7d50a9575222084ee90b1e
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-08 20:05:09 -0300

    Create testando.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula32/testando.java
@@ -0,0 +1,14 @@
+package com.loiane.cursojava.aula32;
+
+public class testando {
+
+	public static void main(String[] args) {
+		Sera van = new Sera();
+		van.setMarca("Fiat"); 
+		
+
+		System.out.println(van.getMarca());
+		
+	}
+	
+}
