797eb5bf9565bb2a1836a1ab73e42be92fbb1b12
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-10 21:53:20 -0300

    Create Contador.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula34/Contador.java
@@ -0,0 +1,28 @@
+package com.loiane.cursojava.aula34;
+
+public class Contador {
+
+	private static int cont;
+
+	public static int getCont(int num) {
+		return cont=num;
+	}
+
+	public static void setCont(int cont) {
+		Contador.cont = cont;
+	}
+	
+	public static void incrementa(int num) {
+		getCont(num);
+	}
+	public static void decremento(int num) {
+		getCont(cont-num);
+	}
+	public static void zerar( ) {
+		setCont(0);
+	}
+	public static void mostra() {
+		System.out.println("O cantador esta em: "+cont);
+	}
+	
+}
