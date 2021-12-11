c8e4baecc83bb406905a0e563fb38596fc16232d
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-10 21:01:07 -0300

    Create Exerc01.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula33/Exerc01.java
@@ -0,0 +1,18 @@
+package com.loiane.cursojava.aula33;
+
+public class Exerc01 {
+
+	public static void main(String[] args) {
+		Lampada lampada = new Lampada();
+		lampada.ligar();
+		lampada.status();
+		lampada.desligar();
+		lampada.status();
+		lampada.mudarEstado();
+		lampada.status();
+		lampada.mudarEstado();
+		lampada.status();
+
+	}
+
+}
