9f9c50c1d311abb3d8ef4c82914e8a516c477726
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-10 21:53:08 -0300

    Create Calculadora.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula34/Calculadora.java
@@ -0,0 +1,24 @@
+package com.loiane.cursojava.aula34;
+
+public class Calculadora {
+	public int soma(int num1, int num2) {
+		return num1+num2;
+	}
+	
+	public static int soma(int num1, int num2,int num3) {
+		return num1+num2+num3;
+	}
+	
+	public static double soma(double num1, double num2) {
+		return num1+num2;
+	}
+	
+	public static int some (int[] vetorinteiro) {
+		int total = 0;
+		
+		for (int i =0 ; i < vetorinteiro.length; i++) {
+			total += vetorinteiro[i];
+		}
+		return total;
+	}
+}
