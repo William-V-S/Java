598d7765b621037db0a84514e29328e7808e82e3
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-10 21:53:32 -0300

    Create Exerc0134.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula34/Exerc0134.java
@@ -0,0 +1,19 @@
+package com.loiane.cursojava.aula34;
+
+
+public class Exerc0134 {
+
+	public static void main(String[] args) {
+		Contador.incrementa(6);
+		Contador.mostra();
+		Contador.incrementa(91);
+		Contador.mostra();
+		Contador.decremento(54);
+		Contador.mostra();
+		Contador.decremento(24);
+		Contador.mostra();
+		Contador.zerar();
+		Contador.mostra();
+	}
+
+}
