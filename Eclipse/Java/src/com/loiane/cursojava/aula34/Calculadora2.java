6a76b021ad5d6ccfc8e0ae3f23dd8c64abc19a86
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-14 18:10:57 -0300

    Create Calculadora2.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula34/Calculadora2.java
@@ -0,0 +1,9 @@
+package com.loiane.cursojava.aula34;
+
+public class Calculadora2 {
+	public static void main(String args[]) {
+		Calculadora calc = new Calculadora();
+		int num = calc.fatorial(10);
+		System.out.println(num);
+	}
+}
