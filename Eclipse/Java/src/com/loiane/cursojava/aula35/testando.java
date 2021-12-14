7bc29e0d889f0490bf5dcdd56e69b0a27cccc492
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-14 18:10:29 -0300

    Create testando.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula35/testando.java
@@ -0,0 +1,13 @@
+package com.loiane.cursojava.aula35;
+
+public class testando {
+
+	public static void main(String[] args) {
+		int fatorialnr = Calculadora.fatorialnr(5);
+		System.out.println(fatorialnr);
+		
+		int fatorial = Calculadora.fatorial(5);
+		System.out.println(fatorial);
+	}
+
+}
