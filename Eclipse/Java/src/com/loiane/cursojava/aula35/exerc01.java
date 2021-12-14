a605e0e5aa20e0105bc23d08afe63b4f7dcdc558
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-14 18:10:41 -0300

    Create exerc01.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula35/exerc01.java
@@ -0,0 +1,14 @@
+package com.loiane.cursojava.aula35;
+
+public class exerc01 {
+
+	public static void main(String[] args) {
+		
+		for (int i =1; i<20;i++) {
+			System.out.print(Fibonaacci.fibonacci(i)+" ");
+			
+		}
+
+	}
+
+}
