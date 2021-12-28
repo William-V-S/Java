ab89b87b9324884ce4032d681cb6978b2e7e3c62
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-28 19:27:32 -0300

    Create Cachorro.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula44/Cachorro.java
@@ -0,0 +1,47 @@
+package com.loiane.cursojava.aula44;
+
+public class Cachorro extends Mamifero implements AnimalDomestico,AnimalDomesticado{
+
+	@Override
+	public void amamentar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void emitirsom() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String alimentar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String levarVeterinario() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String chamarVeterinario() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String brincar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String levarPassear() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
