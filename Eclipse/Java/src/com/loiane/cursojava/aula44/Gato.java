56aa7d5752d79aafab56fedb7a06ddbb27d81a22
William-V-S <74629076+William-V-S@users.noreply.github.com>
2021-12-28 19:27:45 -0300

    Create Gato.java

---
+++ Eclipse/Java/src/com/loiane/cursojava/aula44/Gato.java
@@ -0,0 +1,43 @@
+package com.loiane.cursojava.aula44;
+
+public class Gato extends Mamifero implements AnimalDomestico,AnimalDomesticado{
+
+	@Override
+	public void amamentar() {
+
+	}
+	
+	@Override
+	public void emitirsom() {
+
+	}
+	
+	@Override
+	public String brincar() {
+		return null;
+	}
+	
+	@Override
+	public String levarPassear() {
+		return null;
+	}
+
+	@Override
+	public String alimentar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String levarVeterinario() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String chamarVeterinario() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
